d1f44641307883b6ffe9ac72fbd244189b91edfa
AlejandroECA <alejandroecarrilloa@gmail.com>
2021-10-26 21:32:03 -0500

    direct10

--- calculator/calculator.py
+++ calculator/calculator.py
@@ -8,3 +8,7 @@ def multiply(first_term, second_term):
 
 def subtract(first_term, second_term):
     return first_term - second_term
+    
+ 
+def division(first_term, second_term):
+    return first_term / second_term
